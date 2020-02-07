0f659be81150e592f0dacc9075653c8e740b1ea5
MattRob77 <Matt_Robinson1@yahoo.com>
2020-02-07 09:01:30 -0500

    Set up provider for store and imports with react-redux

--- src/index.js
+++ src/index.js
@@ -2,8 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import App from './App'
 
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import catsReducer from './reducers/catsReducer.js'
+
+const store = createStore(catsReducer)
+
 
 ReactDOM.render(
-    <App />,
-    document.getElementById('root')
+  <Provider store={store}>
+    <APP />
+  </Provider>,
+  document.getElementById('root')
 );
