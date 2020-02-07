8a5c0ec0b366fbb196269ef47694a65a7f7ab259
Doug Kintop <dkintop@hotmail.com>
2020-02-07 14:57:54 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ class App extends Component {
     return (
       <div>
         <h1>CatBook</h1>
-        {/* add CatList component here */}
+        const mapStateToProps = state => {
+  return {
+    catPics: state.cats,
+    loading: state.loading
+  }
+}
       </div>
     );
   }
