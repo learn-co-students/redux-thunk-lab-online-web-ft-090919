3655d3b1d1c815749fb02ea72d41b181f170a69d
Jendnice <Pipeline38@hotmail.com>
2020-02-10 17:34:50 -0500

    Done.

--- src/reducers/catsReducer.js
+++ src/reducers/catsReducer.js
@@ -1,1 +1,28 @@
 // add the catsReducer
+
+const catsReducer = (state = { cats: [], loading: false }, action) => {
+
+    switch (action.type){
+        case 'LOADING_CATS':
+            // loading cats
+            return {
+                ...state,
+                cats: [...state.cats],
+                loading: true 
+            }  
+        
+        case 'ADD_CATS':
+            // add cats
+            return {
+                ...state,
+                cats: action.cats,
+                loading: false
+            }       
+        
+        default: 
+            return state
+    }
+}
+
+
+export default catsReducer 
